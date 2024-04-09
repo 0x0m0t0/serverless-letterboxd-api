f03fe76907b8cffdb1df47b47f30f0e14e4c0813
noodles4u <louisdesmett@gmail.com>
2024-04-09 16:19:29 +0200

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Hono } from "hono";
-import { bearerAuth } from "hono/bearer-auth";
-import { prettyJSON } from "hono/pretty-json";
-import { cors } from "hono/cors";
-import { XMLParser } from "fast-xml-parser";
-import { env } from "hono/adapter";
+import { Hono } from 'hono';
+import { bearerAuth } from 'hono/bearer-auth';
+import { prettyJSON } from 'hono/pretty-json';
+import { cors } from 'hono/cors';
+import { XMLParser } from 'fast-xml-parser';
+import { env } from 'hono/adapter';
 
 type Bindings = {
   TOKEN: string;
@@ -14,7 +14,7 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.use("*", cors());
+app.use('*', cors());
 
 const regex = /<img[^>]*src="([^"]*)"[^>]*>/;
 const regexStars = /[★½]+/g;
@@ -24,6 +24,9 @@ const regexYear = /\b\d{4}\b/;
 const fetchRssFeed = async (url: string, user: string) => {
   try {
     const response = await fetch(url);
+    console.log('Response status:', response.status);
+    console.log('Response URL:', response.url);
+    console.log('Response text:', await response.text());
 
     if (!response.ok) {
       throw new Error(`Failed to fetch XML. Status: ${response.status}`);
@@ -42,13 +45,11 @@ const fetchRssFeed = async (url: string, user: string) => {
         return {
           title: item?.title?.match(regexTitle)?.[1],
           // year: item?.title?.match(regexYear)?.[0],
-          year: item?.["letterboxd:filmYear"],
-          stars: item?.title?.match(regexStars)?.[0]
-            ? item.title.match(regexStars)?.[0]
-            : null,
-          link: item?.link?.replace(user, ""),
+          year: item?.['letterboxd:filmYear'],
+          stars: item?.title?.match(regexStars)?.[0] ? item.title.match(regexStars)?.[0] : null,
+          link: item?.link?.replace(user, ''),
           poster: poster,
-          watchedOn: new Date(item?.["letterboxd:watchedDate"]),
+          watchedOn: new Date(item?.['letterboxd:watchedDate']),
         };
       });
       return data;
@@ -61,24 +62,25 @@ const fetchRssFeed = async (url: string, user: string) => {
   }
 };
 
-app.use("*", prettyJSON());
+app.use('*', prettyJSON());
 
-app.get("/", (c) => c.text("hello welcome to your personal letterboxd api"));
+app.get('/', (c) => c.text('hello welcome to your personal letterboxd api'));
 
 const getTokenFromEnvironment = (c: any) => {
   const { TOKEN } = env<{ TOKEN: string }>(c);
   return TOKEN;
 };
 
-app.use("/api/*", async (c, next) => {
+app.use('/api/*', async (c, next) => {
   const token = getTokenFromEnvironment(c);
   const auth = bearerAuth({
     token: token,
   });
+  console.log('yeeha authorized');
   await auth(c, next);
 });
 
-app.get("/api/feed", async (c, next) => {
+app.get('/api/feed', async (c, next) => {
   try {
     const response = await fetchRssFeed(c.env.URL, c.env.USER);
     console.log(response);
@@ -86,9 +88,13 @@ app.get("/api/feed", async (c, next) => {
   } catch (error) {
     c.json({
       status: 500,
-      message: "Internal Server Error",
+      message: 'Internal Server Error',
     });
   }
 });
 
-export default app;
+// export default app;
+export default {
+  port: 3000,
+  fetch: app.fetch,
+};
